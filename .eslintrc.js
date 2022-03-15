7c3d588469f199316b1ac8291a834f9b557f3206
Jose Alvarez <j.alvarez11@icloud.com>
2022-03-15 13:36:45 -0400

    chore: add eslint + prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+};
