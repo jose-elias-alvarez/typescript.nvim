35a4a67f22ff53ce8846153e1a80d703f84abcc1
lebit <spam@labitte.com>
2023-01-25 14:13:33 +0100

    feat: add command :TypescriptRenameFolder

--- lua/typescript/commands.lua
+++ lua/typescript/commands.lua
@@ -6,6 +6,8 @@ local ____go_2Dto_2Dsource_2Ddefinition = require("typescript.go-to-source-defin
 local goToSourceDefinition = ____go_2Dto_2Dsource_2Ddefinition.goToSourceDefinition
 local ____rename_2Dfile = require("typescript.rename-file")
 local renameFile = ____rename_2Dfile.renameFile
+local ____rename_2Dfolder = require("typescript.rename-folder")
+local renameFolder = ____rename_2Dfolder.renameFolder
 local ____source_2Dactions = require("typescript.source-actions")
 local addMissingImports = ____source_2Dactions.addMissingImports
 local fixAll = ____source_2Dactions.fixAll
@@ -24,6 +26,31 @@ ____exports.setupCommands = function(bufnr)
                         return
                     end
                     renameFile(source, input, {force = opts.bang})
+                end
+            )
+        end,
+        {bang = true}
+    )
+    vim.api.nvim_buf_create_user_command(
+        bufnr,
+        "TypescriptRenameFolder",
+        function(opts)
+            local sourceFile = vim.api.nvim_buf_get_name(bufnr)
+            vim.ui.input(
+                {prompt = "Old path: ", default = sourceFile},
+                function(sourceInput)
+                    if sourceInput == "" or sourceInput == sourceFile or sourceInput == nil then
+                        return
+                    end
+                    vim.ui.input(
+                        {prompt = "New path: ", default = sourceFile},
+                        function(targetInput)
+                            if targetInput == "" or targetInput == sourceFile or targetInput == nil then
+                                return
+                            end
+                            renameFolder(sourceInput, targetInput, {force = opts.bang})
+                        end
+                    )
                 end
             )
         end,
